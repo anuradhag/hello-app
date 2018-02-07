331c775a1764bdb49c4e6e9ec09520b087b87cb1
maknahar <maknahar@live.in>
2018-02-07 11:01:46 +0530

    Added js function that return react element

--- src/App.js
+++ src/App.js
@@ -11,13 +11,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Learn React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <Lakshmi/>
+
+        {getFooter()}
+
       </div>
     );
   }
+}
+
+function getFooter() {
+  return (
+    <div>This is footer component. this is an pure js functions</div>
+  )
 }
 
 
